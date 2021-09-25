ea223624f60bf15e1b12a905adedd7cbcf2ffe0b
AbhijithNT <abhijithnt64@gmail.com>
2021-09-25 08:35:26 +0530

    JSON.stringify()

--- app/plugin/onText.ts
+++ app/plugin/onText.ts
@@ -1,7 +1,7 @@
 import { Composer } from "telegraf";
 
 export const onMessage = Composer.on("text", async (msg) => {
-    let onUpdate = JSON.stringify(msg.message.chat)
+  let onUpdate = JSON.stringify(msg.message, undefined, 4)
   msg.telegram.sendMessage(msg.chat.id, `<pre>${onUpdate}</pre>`, {
     parse_mode: "HTML",
   });
