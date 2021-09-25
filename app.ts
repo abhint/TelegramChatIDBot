56bd60d27130ad51cac4235363e1c4034fde7407
AbhijithNT <abhijithnt64@gmail.com>
2021-09-25 07:57:45 +0530

    File name

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import bot from "./app/bot";
 import { onCommands } from "./app/commands";
-import { onMessage } from "./app/plugin/onmsg";
+import { onMessage } from "./app/plugin/onText";
 (async () => {
   await onCommands();
   bot.use(onMessage)
