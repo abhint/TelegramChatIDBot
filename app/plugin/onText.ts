6daec9115b49ccb2a0d9c1950d8fad02bfdd4c20
AbhijithNT <abhijithnt64@gmail.com>
2021-09-25 09:08:44 +0530

    Bad Request: can't parse entities: Unexpected end tag at byte offset 531

--- app/plugin/onText.ts
+++ app/plugin/onText.ts
@@ -1,8 +1,10 @@
-import { Composer } from "telegraf";
+import { Composer, TelegramError } from "telegraf";
+import { Telegraf } from "telegraf";
 
 export const onMessage = Composer.on("text", async (msg) => {
-  let onUpdate = JSON.stringify(msg.message, undefined, 4)
-  msg.telegram.sendMessage(msg.chat.id, `<pre>${onUpdate}</pre>`, {
-    parse_mode: "HTML",
+  let onUpdate = JSON.stringify(msg.message, undefined, 4);
+
+  msg.telegram.sendMessage(msg.chat.id, ("`"+onUpdate+"`"), {
+    parse_mode: "MarkdownV2",
   });
 });
