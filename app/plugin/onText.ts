531e6bb7d368f044489c0976b1fd1c58f47acd9a
AbhijithNT <abhijithnt64@gmail.com>
2021-09-25 08:24:11 +0530

    Telegram JSON

--- app/plugin/onText.ts
+++ app/plugin/onText.ts
@@ -1,3 +1,8 @@
 import { Composer } from "telegraf";
 
-export const onMessage = Composer.on("text", (msg) => console.log(msg));
+export const onMessage = Composer.on("text", async (msg) => {
+    let onUpdate = JSON.stringify(msg.message.chat)
+  msg.telegram.sendMessage(msg.chat.id, `<pre>${onUpdate}</pre>`, {
+    parse_mode: "HTML",
+  });
+});
