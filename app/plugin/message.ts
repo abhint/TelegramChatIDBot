3990623638bb8bb34f7700f526d7e24b464c27b1
AbhijithNT <abhijithnt64@gmail.com>
2021-09-26 12:17:02 +0530

    -_-

--- app/plugin/message.ts
+++ app/plugin/message.ts
@@ -1,8 +1,7 @@
-import { Composer, TelegramError } from "telegraf";
-import { Telegraf } from "telegraf";
+import { Composer } from "telegraf";
 
 export const onMessage = Composer.on("message", async (msg) => {
-  let onUpdate = JSON.stringify(msg.message.from, undefined, 4);
+  let onUpdate = JSON.stringify(msg.message, undefined, 4);
 
   msg.telegram.sendMessage(msg.chat.id, ("`"+onUpdate+"`"), {
     parse_mode: "MarkdownV2",
