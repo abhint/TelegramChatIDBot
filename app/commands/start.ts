29ccdfa12cd4ae851c47b8e43367885e83e565f7
AbhijithNT <abhijithnt64@gmail.com>
2021-09-27 13:22:52 +0530

    Bad Request: can't parse entities

--- app/commands/start.ts
+++ app/commands/start.ts
@@ -1,5 +1,11 @@
 import { Composer } from "telegraf";
 
 export const onStart = Composer.command("start", async (msg) => {
-  msg.telegram.sendMessage(msg.chat.id, "Hello, World!");
+  msg.telegram.sendMessage(
+    msg.chat.id,
+    "Hello, [" + msg.from.first_name + "](tg://user?id=" + msg.chat.id + ")",
+    {
+      parse_mode: "Markdown",
+    }
+  );
 });
