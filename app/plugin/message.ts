e5ef5ee72abb75bbcf95dec6a4cbb306077b7eb9
AbhijithNT <abhijithnt64@gmail.com>
2021-09-27 16:18:09 +0530

    reply

--- app/plugin/message.ts
+++ app/plugin/message.ts
@@ -3,7 +3,8 @@ import { Composer } from "telegraf";
 export const onMessage = Composer.on("message", async (msg) => {
   let onUpdate = JSON.stringify(msg.message, undefined, 4);
 
-  msg.telegram.sendMessage(msg.chat.id, ("`"+onUpdate+"`"), {
+  msg.telegram.sendMessage(msg.chat.id, "`" + onUpdate + "`", {
+    reply_to_message_id: msg.message.message_id,
     parse_mode: "MarkdownV2",
   });
 });
