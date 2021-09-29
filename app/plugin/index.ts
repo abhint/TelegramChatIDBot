aac80580397f9c39c481fa86deedf5d5115a9883
AbhijithNT <abhijithnt64@gmail.com>
2021-09-29 10:37:41 +0530

    id

--- app/plugin/index.ts
+++ app/plugin/index.ts
@@ -1,7 +1,8 @@
 import { Composer } from "telegraf";
 import bot from "../bot";
 import { onMessage } from "./message";
-
+import { onCommandID } from "./message";
 export const onPlugin = async () => {
+  bot.use(Composer.privateChat(onCommandID));
   bot.use(Composer.privateChat(onMessage));
 };
