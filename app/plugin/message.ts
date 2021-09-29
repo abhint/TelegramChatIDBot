39b684bf7d4f4023169a374431ada388da6bc506
AbhijithNT <abhijithnt64@gmail.com>
2021-09-29 13:40:36 +0530

    improve

--- app/plugin/message.ts
+++ app/plugin/message.ts
@@ -2,34 +2,56 @@ import { Composer } from "telegraf";
 
 export const onCommandID = Composer.command("/id", async (msg) => {
   let onUpdate: any = msg.message;
+  console.log(msg.message);
+
   if (msg.message.reply_to_message == undefined) {
-    msg.telegram.sendMessage(msg.chat.id, "CHAT ID: `" + msg.chat.id + "`", {
-      parse_mode: "Markdown",
-    });
+    msg.telegram.sendMessage(
+      msg.chat.id,
+      "*CHAT ID:* `" + msg.chat.id + "`\n" + "*USER ID: *`" + msg.from.id + "`",
+      {
+        parse_mode: "Markdown",
+      }
+    );
   } else {
     let onID: any;
     if (
       onUpdate.reply_to_message.forward_from_chat === undefined &&
       onUpdate.reply_to_message.forward_from === undefined
     ) {
-      onID = "USER ID: `" + onUpdate.from.id + "`";
+      onID =
+        "*CHAT ID: *`" +
+        onUpdate.chat.id +
+        "`\n" +
+        "*USER ID:* `" +
+        onUpdate.from.id +
+        "`";
     } else if (onUpdate.reply_to_message.forward_from === undefined) {
       onID =
-        "FORWARD CHAT ID: `" +
+        "*CHAT ID: *`" +
+        onUpdate.chat.id +
+        "`\n" +
+        "*FORWARD CHAT ID:* `" +
         onUpdate.reply_to_message.forward_from_chat.id +
+        "`\n" +
+        "*USER ID:* `" +
+        onUpdate.from.id +
         "`";
     } else {
       onID =
-        "FORWARD CHAT ID: `" + onUpdate.reply_to_message.forward_from.id + "`";
+        "*CHAT ID: *`" +
+        onUpdate.chat.id +
+        "`\n" +
+        "*FORWARD CHAT ID:* `" +
+        onUpdate.reply_to_message.forward_from.id +
+        "`\n" +
+        "*USER ID:* `" +
+        onUpdate.from.id +
+        "`";
     }
-    msg.telegram.sendMessage(
-      msg.chat.id,
-      "CHAT ID: `" + onUpdate.from.id + "`\n" + onID,
-      {
-        reply_to_message_id: msg.message.message_id,
-        parse_mode: "MarkdownV2",
-      }
-    );
+    msg.telegram.sendMessage(msg.chat.id, onID, {
+      reply_to_message_id: msg.message.message_id,
+      parse_mode: "MarkdownV2",
+    });
   }
 });
 
