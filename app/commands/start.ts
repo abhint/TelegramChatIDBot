65546825227d89381fe5e234d8950f463dfaa8ff
AbhijithNT <abhijithnt64@gmail.com>
2021-11-06 17:33:59 +0530

    message error

--- app/commands/start.ts
+++ app/commands/start.ts
@@ -7,14 +7,10 @@ export const onStart = Composer.command("start", async (msg) => {
       msg.from.first_name +
       "](tg://user?id=" +
       msg.chat.id +
-      ")\n*എന്തിനോ വേണ്ടി തിളക്കുന്ന സാംബാർ*",
+      ")\n*എന്തിനോ വേണ്ടി തിളക്കുന്ന സാംബാർ*\nhttps://telesco.pe/AbhijithNT/1427",
     {
       reply_to_message_id: msg.message.message_id,
       parse_mode: "Markdown",
     }
-  );
-  msg.telegram.sendVideoNote(
-    msg.chat.id,
-    "DQACAgEAAxkBAAIXdWFRkLreYlasweaVSaEIeV_YsYjiAAISAgAC35KIRhJsY6fAcCWZIQQ",
   );
 });
