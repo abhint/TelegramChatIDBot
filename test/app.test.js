a05920a69cba74912f0592b260cdeb4e4cdcf58c
AbhijithNT <abhijithnt64@gmail.com>
2022-01-28 21:10:41 +0530

    patch

--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,8 @@
 const bot = require("../app/bot.js");
 
 it("telegram bot test", async () => {
-  const test_result  = await bot.default.launch();
-  expect(test_result).toBe(undefined)
+  const test_result = await bot.default.launch();
+  expect(test_result).toBe(undefined);
 });
 afterAll(() => {
   bot.default.stop("unspecified");
